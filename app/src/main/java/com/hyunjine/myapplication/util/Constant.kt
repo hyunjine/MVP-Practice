20b9b999bc08a45577f73fa367d9d939d20619e0
winter <winter@luple.co.kr>
2022-03-20 18:50:36 +0900

    Hide API key

--- app/src/main/java/com/hyunjine/myapplication/util/Constant.kt
+++ app/src/main/java/com/hyunjine/myapplication/util/Constant.kt
@@ -2,7 +2,7 @@ package com.hyunjine.myapplication.util
 
 object Constant {
     const val BASE_URL = "http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/"
-    const val API_KEY = "nTWVI5VUTYTLm8PyyQrUpx4zVvUiehvnXYQrwwAkPf6nERNfHeY3LL829ZfoTMnbetcMYsT9aO6IoeCiuvftNQ%3D%3D"
+    const val API_KEY = "YOUR KEY"
     const val APP_OS = "AND"
     const val APP_NAME = "Camping"
     const val TYPE = "json"
